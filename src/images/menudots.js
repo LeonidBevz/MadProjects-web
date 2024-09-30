063b9bd63a3b0a6a11cfe137135fd2a8ec9a3630
Группа 4215 Бевз Л.А. <leonidbevz666@gmail.com>
2024-10-01 02:08:07 +0300

    Kanban add cards rows. delete cards rows modal.

--- src/images/menudots.js
+++ src/images/menudots.js
@@ -1,9 +1,48 @@
-const MenuDotsIco = ({ color = "white",className = ""}) => (
-    <svg className = {`${className} svg-trans`} viewBox="0 0 36 35" fill={color} xmlns="http://www.w3.org/2000/svg">
-      <path fillRule="evenodd" clipRule="evenodd" d="M17.6981 6.198C16.2873 6.198 15.1436 7.3406 15.1436 8.75008C15.1436 10.1596 16.2873 11.3022 17.6981 11.3022H17.7127C19.1235 11.3022 20.2672 10.1596 20.2672 8.75008C20.2672 7.3406 19.1235 6.198 17.7127 6.198H17.6981ZM15.1436 17.5001C15.1436 16.0906 16.2873 14.948 17.6981 14.948H17.7127C19.1235 14.948 20.2672 16.0906 20.2672 17.5001C20.2672 18.9096 19.1235 20.0522 17.7127 20.0522H17.6981C16.2873 20.0522 15.1436 18.9096 15.1436 17.5001ZM15.1436 26.2501C15.1436 24.8406 16.2873 23.698 17.6981 23.698H17.7127C19.1235 23.698 20.2672 24.8406 20.2672 26.2501C20.2672 27.6596 19.1235 28.8022 17.7127 28.8022H17.6981C16.2873 28.8022 15.1436 27.6596 15.1436 26.2501Z"/>
-    </svg>
+import React, {useState,useRef, useEffect} from "react";
 
+const MenuDotsIco = ({ color = "white", className = "", onEdit = ()=>{}, onDelete= ()=>{}}) => {
+  const [isMenuOpen,setIsMenuOpen] = useState()
+  const buttonRef = useRef()
+  const menuRef = useRef()
 
-  );
+  const handleDelete = () =>{
+    setIsMenuOpen(false)
+    onDelete()
+  }  
+  const handleEdit = () =>{
+    setIsMenuOpen(false)
+    onEdit()
+  }  
+  useEffect(() => {
+    const handleClickOutside = (event) => {
+      if (menuRef.current  && buttonRef.current && !(menuRef.current.contains(event.target) || buttonRef.current.contains(event.target))) {
+        setIsMenuOpen(false);
+      }
+    };
+
+    if (isMenuOpen) {
+      document.addEventListener('mousedown', handleClickOutside);
+    } else {
+      document.removeEventListener('mousedown', handleClickOutside);
+    }
+
+    return () => {
+      document.removeEventListener('mousedown', handleClickOutside);
+    };
+  }, [isMenuOpen]);
+
+  return(
+    <div className="drop-menu">
+      <svg className = {`${className} svg-trans`} onClick={()=>setIsMenuOpen(!isMenuOpen)} ref={buttonRef} viewBox="0 0 36 35" fill={color} xmlns="http://www.w3.org/2000/svg">
+        <path fillRule="evenodd" clipRule="evenodd" d="M17.6981 6.198C16.2873 6.198 15.1436 7.3406 15.1436 8.75008C15.1436 10.1596 16.2873 11.3022 17.6981 11.3022H17.7127C19.1235 11.3022 20.2672 10.1596 20.2672 8.75008C20.2672 7.3406 19.1235 6.198 17.7127 6.198H17.6981ZM15.1436 17.5001C15.1436 16.0906 16.2873 14.948 17.6981 14.948H17.7127C19.1235 14.948 20.2672 16.0906 20.2672 17.5001C20.2672 18.9096 19.1235 20.0522 17.7127 20.0522H17.6981C16.2873 20.0522 15.1436 18.9096 15.1436 17.5001ZM15.1436 26.2501C15.1436 24.8406 16.2873 23.698 17.6981 23.698H17.7127C19.1235 23.698 20.2672 24.8406 20.2672 26.2501C20.2672 27.6596 19.1235 28.8022 17.7127 28.8022H17.6981C16.2873 28.8022 15.1436 27.6596 15.1436 26.2501Z"/>
+      </svg>
+      {isMenuOpen && (<div className="ddmenu" ref={menuRef} draggable="false">
+        <button onClick={handleEdit}>Редактировать</button>
+        <button className="delete-button" onClick={handleDelete}>Удалить</button>
+      </div>
+      )}
+    </div>
+    )
+  };
   
   export default MenuDotsIco;
--- src/pages/kanbanpage.js
+++ src/pages/kanbanpage.js
@@ -1,38 +1,111 @@
-import React, { useEffect, useState } from "react";
+import React, {  useState } from "react";
 import MenuDotsIco from "../images/menudots";
 import "./kanbanpage.css";
 import useToken from "../hooks/useToken";
 import { DragDropContext, Draggable, Droppable } from "react-beautiful-dnd";
+import PlusIco from "../images/plus";
 
 function formatDate(date) {
   const dateParts = date.slice(0, 10).split('-'); 
   const formattedDate = `${dateParts[2]}.${dateParts[1]}.${dateParts[0]}`; 
   return formattedDate;
 }
-
+const DeleteModal = ({text, onDelete, onCancel}) => {
+  return (
+    <div className="delete-modal bg-trans">
+      <p>{text}</p>
+      <div className="flex-butt">
+        <button className="deletem-button" onClick={onDelete}>Подтвердить</button>
+        <button className="all-trans" onClick={onCancel}>Отмана</button>
+      </div>
+    </div>
+  )
+}
 const KanbanPage = () => {
-  const { isNightTheme } = useToken();
+  const { username, isNightTheme } = useToken();
+  const [isDeleteWindowShown, setIsDeleteWindowShown] = useState(0)
+  const [rowToDelete, setRowToDelete] = useState(null)
+  const [cardToDelete, setCardToDelete] = useState(null)
   const [cards, setCards] = useState([
     {
       rowName: "Пример1",
-      id: "1",
+      id: "row1",
       cards: [
-        { name: "Фикс поиска", id: "1-1", author: "Ilya Bundelev", created: "2024-09-28T12:30:00Z", updated: "2024-09-28T12:30:00Z" },
-        { name: "Фикс поиска", id: "1-2", author: "Ilya Bundelev", created: "2024-09-28T12:30:00Z", updated: "2024-09-28T12:30:00Z" },
+        { name: "Фикс поиска", id: "card1", author: "Ilya Bundelev", created: "2024-09-28T12:30:00Z", updated: "2024-09-28T12:30:00Z" },
+        { name: "Фикс поиска", id: "card2", author: "Ilya Bundelev", created: "2024-09-28T12:30:00Z", updated: "2024-09-28T12:30:00Z" },
       ],
     },
     {
       rowName: "Пример2",
-      id: "2",
+      id: "row2",
       cards: [
-        { name: "Фикс поиска", id: "2-1", author: "Ilya Bundelev", created: "2024-09-28T12:30:00Z", updated: "2024-09-28T12:30:00Z" },
-        { name: "Фикс поиска", id: "2-2", author: "Ilya Bundelev", created: "2024-09-28T12:30:00Z", updated: "2024-09-28T12:30:00Z" },
+        { name: "Фикс поиска", id: "card3", author: "Ilya Bundelev", created: "2024-09-28T12:30:00Z", updated: "2024-09-28T12:30:00Z" },
+        { name: "Фикс поиска", id: "card4", author: "Ilya Bundelev", created: "2024-09-28T12:30:00Z", updated: "2024-09-28T12:30:00Z" },
       ],
     },
   ]);
-  useEffect (()=>{
-    console.log(cards)
-  },[cards])
+
+  const [rowsCount,setRowsCount] = useState(cards.length)
+  const [cardsCount,setCardsCount] = useState(cards.reduce((total, row) => total + row.cards.length, 0))
+  
+  const addColumn = () =>{
+    const newId = (rowsCount + 1).toString(); 
+    setRowsCount(rowsCount + 1)
+    const newColumn = {
+      rowName: `Столбец${newId}`,
+      id: newId,
+      cards: [], 
+    };
+    setCards((prevCards) => [...prevCards, newColumn]);
+  }
+  const addCard = (rowIndex) =>{
+    setCardsCount(cardsCount+1)
+    const newCardId = `card${cardsCount + 1}`
+    
+    const newCard = {
+      name: `Карточка ${newCardId}`,
+      id: newCardId,
+      author: username, 
+      created: new Date().toISOString(),
+      updated: new Date().toISOString(),
+    };
+  
+    setCards((prevCards) => {
+      const updatedCards = [...prevCards];
+      updatedCards[rowIndex].cards.push(newCard); 
+      return updatedCards;
+    });
+  }
+  const deleteRow = (rowId) => {
+    const updatedCards = [...cards];
+    updatedCards.splice(rowId, 1);
+    setCards(updatedCards);
+    setIsDeleteWindowShown(0)
+  };
+  const handleDeleteRowClick = (rowId)=>{
+    setRowToDelete(rowId)
+    setIsDeleteWindowShown(1)
+  }
+
+  const deleteCard = (rowIndex, cardIndex) => {
+    setCards((prevCards) =>
+      prevCards.map((row, i) => {
+        if (i === rowIndex) {
+          const newCards = row.cards.filter((_, j) => j !== cardIndex);
+          
+          return { ...row, cards: newCards };
+        }
+        return row;
+      })
+    );
+    setIsDeleteWindowShown(0)
+  };
+  const handleDeleteCardClick = (rowId, cardId)=>{
+    setRowToDelete(rowId)
+    setCardToDelete(cardId)
+    setIsDeleteWindowShown(2)
+  }
+ 
 
   const [isDragging, setIsDragging] = useState(false); // Для блокирования одновременного перетаскивания
 
@@ -110,6 +183,9 @@ const KanbanPage = () => {
 
   return (
     <div className="kanban-page">
+      <div className={isDeleteWindowShown !==0 ? "bg-blur-shown" :"bg-blur-hidden"}/>
+      {isDeleteWindowShown === 1 && (<DeleteModal onDelete={()=>deleteRow(rowToDelete)} text={`Вы уверены что хотите удалить столбец ${cards[rowToDelete].rowName} с ${cards[rowToDelete].cards.length} карточками?`} onCancel={()=>{setIsDeleteWindowShown(0)}}/>)}
+      {isDeleteWindowShown === 2 && (<DeleteModal onDelete={()=>deleteCard(rowToDelete,cardToDelete)} text={`Вы уверены что хотите удалить карточку ${cards[rowToDelete].cards[cardToDelete].name}?`} onCancel={()=>{setIsDeleteWindowShown(0)}}/>)}
       <DragDropContext onDragEnd={onDragEnd} onDragStart={onDragStart}>
         <Droppable droppableId="all-columns" direction="horizontal" type="column">
           {(provided) => (
@@ -120,7 +196,7 @@ const KanbanPage = () => {
                     <div {...provided.draggableProps} ref={provided.innerRef} className="kanban-row">
                       <div className="kanban-row-top bg-trans" {...provided.dragHandleProps}>
                         <p>{row.rowName}</p>
-                        <MenuDotsIco color={isNightTheme ? "#d4d3cf" : "black"} className="dots" />
+                        <MenuDotsIco color={isNightTheme ? "#d4d3cf" : "black"} className="dots" onDelete={()=>handleDeleteRowClick(i)} />
                       </div>
                       <Droppable droppableId={row.id} type="card">
                         {(provided) => (
@@ -132,7 +208,7 @@ const KanbanPage = () => {
                                     <div className="card-top">
                                       <p className="card-name">{card.name}</p>
                                       <div className="more-butt" >
-                                        <MenuDotsIco color={isNightTheme ? "#d4d3cf" : "black"} className="dots" />
+                                        <MenuDotsIco color={isNightTheme ? "#d4d3cf" : "black"} className="dots" onDelete={()=>handleDeleteCardClick(i,j)}/>
                                       </div>
                                     </div>
                                     <p className="card-info">{`Автор: ${card.author}`}</p>
@@ -143,6 +219,12 @@ const KanbanPage = () => {
                               </Draggable>
                             ))}
                             {provided.placeholder}
+                            <div className="kanban-row">
+                              <div className="kanban-row-top bg-trans pointer" onClick={()=>addCard(i)}>
+                                <p className="kanban-add-text">Добавить карточку</p>
+                                <PlusIco color={isNightTheme ? "#d4d3cf" : "black"} className="dots"/>
+                              </div>
+                            </div>
                           </div>
                         )}
                       </Droppable>
@@ -151,6 +233,12 @@ const KanbanPage = () => {
                 </Draggable>
               ))}
               {provided.placeholder}
+              <div className="kanban-row">
+                <div className="kanban-row-top bg-trans pointer" onClick={addColumn}>
+                  <p>Добавить столбец</p>
+                  <PlusIco color={isNightTheme ? "#d4d3cf" : "black"} className="dots"/>
+                </div>
+              </div>
             </div>
           )}
         </Droppable>
